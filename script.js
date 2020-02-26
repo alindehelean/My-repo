908a0ffa525a4894501f3079561147f104f64605
Alin Dehelean <alyndehelean@gmail.com>
2020-02-26 11:54:53 +0200

    files added

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("start");
+
+let b;
+b = "pear"
+console.log(b);
+
+b = "cirese";
+console.log(b);
+
+let a = "mere";
+console.log(a);
+
+//let a = 1, b = 2, c = 3;
+
+const c = "tomatoes";
+console.log(c);
+//c = "rosii";
+
+let whatTypeIsThis = "we can find out";
+console.log(typeof whatTypeIsThis);
+
+let operatorLeft = "My name";
+let operatorRight = "Stefy";
+
+let stringConcatenation = operatorLeft + " is " + operatorRight;
+
+let string_interpolation = `${operatorLeft} is ${operatorRight}`;
+console.log(string_interpolation);
+
+let addition = number +2;
